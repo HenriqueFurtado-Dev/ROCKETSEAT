8ef4c41cacea8122bb85887bb20fd19c1ae35c91
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2022-09-01 18:00:25 -0300

    Add files via upload

---
+++ Notions/functions.js
@@ -0,0 +1,23 @@
+console.log("---------------------------------")
+
+// Criando um aplicativo com frases motivacionais
+
+
+// Ela é apenas criada, e só ficará ativa quando for chamada
+// Criando a funcão - declarando a função
+function createPhrases() {
+    console.log('Estudar é: Paixão')
+    console.log('Estudar é: Amor')
+    console.log('Estudar é: Desejo')    
+}
+
+// Executando uma função
+createPhrases()
+
+// Colocando uma função dentro de uma variável
+const sum = function(number1, number2) {
+    // Esses valores serão exibidos somente aqui
+}
+
+// Atribuindo valor para os valores dentro da função
+sum(2, 3) // Argumentos
